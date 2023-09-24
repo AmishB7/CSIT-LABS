01b89ee222134a4c2e44e89d98d7ebfccfcba252
Amish Bajracharya <amishbajracharya63@gmail.com>
2023-09-24 10:02:25 +0545

    Login&Signup

---
+++ Web-technology/Login&singup/homep.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+
+
+<body>
+    <P> Welcome to Home Page !!!<P>
+    <P>To Signup new account <a href = "register.php">Click Here <a></p>
+    <p> To log in another account <a href = "signin.php"> Click Here</a></p>
+</body>
+</html>
